a7c3e91b63297d996a2165c81917bcb29b97feb0
Rammamus <adrian.folin@elev.ga.lbs.se>
2024-02-07 11:27:00 +0100

    ababa

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //All variable relative to the movement - Adrian
+    [Header("Movement")]
     public float moveSpeed;
     public float groundDrag;
+    public float jumpForce;
+
+    [Header("Jumping")]
+    public float maxFallSpeed;
+    public bool grounded;
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    public float fallingGravity;
+
+    public float gravityScale = 1.0f;
+    public static float globalGravity = -9.81f;
 
     public Transform orientation;
     float horizontalInput;
@@ -16,20 +28,27 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
+    CustomGravity customGravity;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        rb.useGravity = false;
     }
 
     private void FixedUpdate()
     {
         MovePlayer();
+        //Makes the player actually fucking use gravity - Adrian
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rb.AddForce(gravity, ForceMode.Acceleration);
     }
 
     private void Update()
     {
         MyInput();
+        Jump();
     }
 
     //Checks the player's movement inputs - Adrian
@@ -44,5 +63,19 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput; //Declares the players movedirection with the orientation (which way the camera is facing) and the players inputs - Adrian
         rb.drag = groundDrag;
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); //Adds force in the previously calculated movedirection - Adrian
+    }
+
+    private void Jump()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce * 10, ForceMode.Force);
+        }
+        if (rb.velocity.y < 0)
+        {
+            gravityScale *= fallingGravity;
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
     }
 }
