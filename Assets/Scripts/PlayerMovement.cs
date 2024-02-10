33d0da582e2fa279427bfc84eab10384959c8bba
Rammamus <adrian.folin@elev.ga.lbs.se>
2024-02-10 16:59:25 +0100

    jkaboom

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region All Variables
     [Header("Movement")]
     public float moveSpeed;
     public float groundDrag;
 
-    [Header("Grounding")]
+    [Header("Ground And Wall Check")]
     public bool grounded;
+    public bool wallToLeft;
+    public bool wallToRight;
+    public bool canWallRun;
+    public bool isWallRunning;
     public float playerHeight;
+    public float playerWidth;
     public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
 
     [Header("Dashing")]
     public float dashForce;
     public int dashes;
     public float dashCooldown;
     public float dashTimer;
+    public Slider sliderL, sliderR;
+    public Image imageL, imageR;
 
     [Header("Jumping")]
     public bool canJump;
+    public bool canDoubleJump;
     public bool isJumping;
     public float jumpForce;
 
@@ -41,10 +52,10 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 moveDirection;
     Rigidbody rb;
+    #endregion
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
         rb.useGravity = false;
         gravityScale = 1;
         dashes = 2;
@@ -60,24 +71,87 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        GroundCheck();
+        GroundWallCheck();
         MyInput();
-        Jump();
-        if (Input.GetKeyDown(KeyCode.LeftShift) && dashes > 0)
+        
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("shift");
-            Dash();
+            if(canJump || canDoubleJump){
+                Jump();
+            }
         }
 
-        if (dashes < 2)
+        if (Input.GetKey(KeyCode.Space)){
+            if (!grounded){
+                canWallRun = true;
+            }
+
+            if(canWallRun){
+                if(wallToLeft){
+                    LeftWallRun();
+                }
+                else if(wallToRight){
+                    RightWallRun();
+                }
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && isWallRunning){
+            Debug.Log("<color=green>boing</color>");
+            isWallRunning = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashes > 0)
+        {
+            Dash();
+        }
+        
+
+        #region Dash timer and cooldown + UI
+        if (dashes == 2)
+        {
+            sliderL.value = sliderR.value = 1;
+            imageL.color = imageR.color = new Color32(43, 190, 224, 225); //blue
+        }
+        else if (dashes < 2)
         {
             dashTimer += Time.deltaTime;
+            if(dashes == 1){
+                sliderL.value = 1;
+                sliderR.value = dashTimer/dashCooldown;
+                imageL.color = new Color32(43, 190, 224, 225); //blue
+                imageR.color = new Color32(255, 255, 255, 225); //white
+            }
+            else if (dashes == 0){
+                sliderL.value = dashTimer/dashCooldown;
+                sliderR.value = 0;
+                imageL.color = new Color32(255, 255, 255, 225); //white
+                imageR.color = new Color32(255, 255, 255, 225); //white
+            }
+            
             if (dashTimer > dashCooldown)
             {
                 dashes++;
                 dashTimer = 0;
             }
         }
+        #endregion
+
+        #region Gravity and falling
+        if (rb.velocity.y < 0)
+        {
+            SetGravityScale(fallingGravity);
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed), rb.velocity.z);
+        }
+        else if (rb.velocity.y < hangTimeFallingThreshold && rb.velocity.y > hangTimeRisingThreshold && !grounded)
+        {
+            SetGravityScale(hangTimeGravity);
+        }
+        else if (grounded)
+        {
+            SetGravityScale(1);
+            isJumping = false;
+        }
+        #endregion
     }
 
     #region Move Player
@@ -99,33 +173,31 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.AddForce(camera.transform.forward * dashForce, ForceMode.Impulse);
         dashes--;
-        Debug.Log("dash");
+    }
+
+    private void LeftWallRun()
+    {
+        isWallRunning = true;
+        Debug.Log("<color=blue>left wallrun</color>");
+    }
+    private void RightWallRun(){
+        isWallRunning = true;
+        Debug.Log("<color=red>right wallrun</color>");
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
-        {
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-            isJumping = true;
+        if(canJump && canDoubleJump){
+            canJump = false;
         }
-
-        #region Gravity and falling
-        if (rb.velocity.y < 0)
-        {
-            SetGravityScale(fallingGravity);
-            rb.velocity = new Vector3(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed), rb.velocity.z);
-        }
-        else if (rb.velocity.y < hangTimeFallingThreshold && rb.velocity.y > hangTimeRisingThreshold && !grounded)
-        {
-            SetGravityScale(hangTimeGravity);
-        }
-        else if (grounded)
-        {
+        else if(!canJump && canDoubleJump){
+            canDoubleJump = false;
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); //Sets falling velocity to 0 so you don't get a fucked up doublejump sometimes
             SetGravityScale(1);
-            isJumping = false;
         }
-        #endregion
+            
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        isJumping = true;
     }
 
     #region General Methods
@@ -134,17 +206,35 @@ public class PlayerMovement : MonoBehaviour
         gravityScale = scale;
     }
 
-    public void GroundCheck()
+    public void GroundWallCheck()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
+        Vector3 playerSide = orientation.transform.right;
+        wallToLeft = Physics.Raycast(transform.position, -playerSide, playerWidth * 0.5f, whatIsWall);
+        wallToRight = Physics.Raycast(transform.position, playerSide, playerWidth * 0.5f, whatIsWall);
 
+
+        //debugging
+        Debug.DrawRay(transform.position, Vector3.down * playerHeight, Color.green); // Ray down
+        Debug.DrawRay(transform.position, -playerSide * playerWidth, Color.red); // Ray to the left
+        Debug.DrawRay(transform.position, playerSide * playerWidth, Color.blue); // Ray to the right
+        
+        
         if (grounded)
         {
             canJump = true;
+            canDoubleJump = true;
+            canWallRun = false;
         }
         else
         {
             canJump = false;
+        }
+
+        if (wallToLeft){
+        }
+
+        if (wallToRight){
         }
     }
     #endregion
